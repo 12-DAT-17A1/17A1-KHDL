aaa311bbf7e525f203c7171256a1e186ac615bf4
NGUYEN TIEN DAT <nguyendat062506@gmail.com>
2023-11-11 13:25:17 +0700

    Bài tập chương 6

--- C06/BT 1.8.py
+++ C06/BT 1.8.py
@@ -6,7 +6,7 @@ print(f"""
 Chiều dài chuỗi s1 = {len(a)}
 Chiều dài chuỗi s2 = {len(b)}
 Chiều dài chuỗi s3 = {len(c)}""")
-print("Chuỗi s4 = {}".format(a[2:5]))
+print("Chuỗi s4 = {}".format(a[d:]))
 print("Chuỗi s2 lặp lại 2 lần = {} ".format(b*2))
 
 
